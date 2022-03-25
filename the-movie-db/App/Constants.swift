b4ba1782bca17d72ed834e20c46219c56affa306
KuKu <khukhu113@gmail.com>
2022-03-25 10:06:35 +0630

    upload commit

---
+++ the-movie-db/App/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  AppConstants.swift
+//  iOS-challenge-BTG
+//
+//  Created by Bruno on 17/01/20.
+//  Copyright © 2020 Bruno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    struct coreData {
+        let movie = "CoreDataMovie"
+    }
+
+    struct api {
+        let key = "db85bc6bf1d96e2f47ac91af80e1d717"
+        let url = "https://api.themoviedb.org/3"
+    }
+
+    struct color {
+        let darkGreen = UIColor(rgb: 0x081C25)
+        let lightGreen = UIColor(rgb: 0x02D475)
+    }
+}
